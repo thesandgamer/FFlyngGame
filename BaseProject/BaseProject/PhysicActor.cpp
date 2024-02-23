ef39080b9ff0075504b5ab786cf6f3e2369bfd51
anouri <anourigat@artfx.fr>
2024-02-23 11:04:20 +0100

    Fix

--- BaseProject/BaseProject/PhysicActor.cpp
+++ BaseProject/BaseProject/PhysicActor.cpp
@@ -23,9 +23,9 @@ void PhysicActor::Start()
 	pb.SetColliderForCollisionCheck(GetCollision());//Set la boite de collision pour le check des cols de la gravité
 	pb.SetBounciness(1);
 	pb.friction = 0;
-	//pb.canFall = true;
+	pb.canFall = true;
 
-	pb.AddForce({ -200,0 ,0 });
+	pb.AddForce({ 500,0 ,0 });
 	//pb.AddForce({ 0,-200 ,0 });
 
 
